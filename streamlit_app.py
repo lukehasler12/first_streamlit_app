442ba44b5cf3d36ebe23f6c26ea27c5f3e12a7b4
Luke Hasler <87906003+lukehasler12@users.noreply.github.com>
2023-09-29 15:26:04 +0100

    Update streamlit_app.py
    Added import for URLError

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title("My Mum's New Healthy Diner")
 
@@ -34,6 +35,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output the data in a dataframe
 streamlit.dataframe(fruityvice_normalized)
+
+# dont run anything past here while we troubleshoot
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
