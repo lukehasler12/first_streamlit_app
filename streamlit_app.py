719d88c611e9ed02108b15327fa1b9c17ecce0ce
Luke Hasler <87906003+lukehasler12@users.noreply.github.com>
2023-09-29 15:10:51 +0100

    Update streamlit_app.py
    Fetching each item and I made the titles a bit more appealing (as in banana - get it???)

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import pandas
 import requests
 import snowflake.connector
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mum's New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
@@ -38,9 +38,9 @@ streamlit.dataframe(fruityvice_normalized)
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
 
 
 
